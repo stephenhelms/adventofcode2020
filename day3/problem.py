d7c12722282833e14be5e82866d01acee754131b
Stephen Helms <stephenhelms@gmail.com>
2020-12-20 22:17:28 +0100

    day 3 problem 2 solution

--- day3/problem.py
+++ day3/problem.py
@@ -1,18 +1,30 @@
+from functools import reduce
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 LANDSCAPE = Path(__file__).parent / 'dataset.txt'
 
 
 def main():
-    landscape = LANDSCAPE.read_text()
+    landscape = parse_landscape(LANDSCAPE.read_text())
     print('Problem 1: ', count_trees_hit(landscape, 3, 1))
+    print('Problem 2: ', multiply_trees_hit_on_angles(landscape, [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ]))
 
 
-def count_trees_hit(landscape: str, num_right: int, num_down: int) -> int:
-    parsed_landscape = parse_landscape(landscape)
-    landscape_x_dim = len(parsed_landscape[0])
-    landscape_y_dim = len(parsed_landscape)
+def multiply_trees_hit_on_angles(landscape: List[str], angles: Tuple[int, int]) -> int:
+    trees_hit = (count_trees_hit(landscape, *angle) for angle in angles)
+    return reduce(lambda agg, v: agg * v, trees_hit)
+
+
+def count_trees_hit(landscape: List[str], num_right: int, num_down: int) -> int:
+    landscape_x_dim = len(landscape[0])
+    landscape_y_dim = len(landscape)
     x, y = 0, 0
     num_trees = 0
     while y < landscape_y_dim:
@@ -22,7 +34,7 @@ def count_trees_hit(landscape: str, num_right: int, num_down: int) -> int:
         y += num_down
         if y >= landscape_y_dim:
             break
-        if parsed_landscape[y][x] == '#':
+        if landscape[y][x] == '#':
             num_trees += 1
     return num_trees
 
