60a2d850794a8c36ca34bd883a8bdfcbce38ba33
Stephen Helms <stephenhelms@gmail.com>
2020-12-20 11:25:30 +0100

    day2 problem 2 - refactor

--- day2/problem.py
+++ day2/problem.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pathlib import Path
 import re
 from typing import List, NamedTuple
@@ -5,14 +6,20 @@ from typing import List, NamedTuple
 DATASET = Path(__file__).parent / 'dataset.txt'
 
 
+class Constraint(Enum):
+    problem1 = 1
+    problem2 = 2
+
+
 def main():
     passwords = read_dataset(DATASET)
-    print('Problem 1: ', solve_problem(passwords))
+    print('Problem 1: ', solve_problem(passwords, Constraint.problem1))
+    print('Problem 1: ', solve_problem(passwords, Constraint.problem2))
 
 
 class Password(NamedTuple):
-    min_occurs: int
-    max_occurs: int
+    constraint1: int
+    constraint2: int
     letter: str
     password: str
 
@@ -30,15 +37,18 @@ def read_dataset(filename: Path) -> List[Password]:
     return passwords
 
 
-def solve_problem(passwords: List[Password]) -> int:
-    return sum(1 for password in passwords if validate_password(password))
+def solve_problem(passwords: List[Password], constraint_type: Constraint) -> int:
+    return sum(1 for password in passwords if validate_password(password, constraint_type))
 
 
-def validate_password(password: Password) -> bool:
-    char_count = sum(1 for char in password.password if char == password.letter)
-    if password.min_occurs <= char_count <= password.max_occurs:
-        return True
-    return False
+def validate_password(password: Password, constraint_type: Constraint) -> bool:
+    if constraint_type == Constraint.problem1:
+        char_count = sum(1 for char in password.password if char == password.letter)
+        if password.constraint1 <= char_count <= password.constraint2:
+            return True
+        return False
+    else:
+        raise NotImplementedError(constraint_type)
 
 
 if __name__ == '__main__':
--- day2/test_day2.py
+++ day2/test_day2.py
@@ -1,4 +1,4 @@
-from problem import DATASET, Password, read_dataset, solve_problem, validate_password
+from problem import Constraint, DATASET, Password, read_dataset, solve_problem, validate_password
 
 PASSWORDS = [
     Password(1, 3, 'a', 'abcde'),
@@ -8,7 +8,7 @@ PASSWORDS = [
 
 
 def test_day2_problem1_solve():
-    assert solve_problem(PASSWORDS) == 2
+    assert solve_problem(PASSWORDS, Constraint.problem1) == 2
 
 
 def test_day2_read_dataset():
@@ -17,6 +17,6 @@ def test_day2_read_dataset():
     assert len(passwords) == 1000
 
 
-def test_day2_validate_password():
-    assert validate_password(PASSWORDS[0])
-    assert not validate_password(PASSWORDS[1])
+def test_day2_problem1_validate_password():
+    assert validate_password(PASSWORDS[0], Constraint.problem1)
+    assert not validate_password(PASSWORDS[1], Constraint.problem1)
