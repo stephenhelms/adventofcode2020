bd10f9247021d60c832e2fd8aa4f611a3728699c
Stephen Helms <stephenhelms@gmail.com>
2020-12-23 12:07:33 +0100

    day 11 problem 2

--- day11/problem.py
+++ day11/problem.py
@@ -1,21 +1,25 @@
 from enum import Enum
 from pathlib import Path
-from typing import Iterable, List
+from typing import Callable, Iterable, List
 
 DATASET = Path(__file__).parent / 'dataset.txt'
 
 
 def main():
-    current_layout = parse_layout(DATASET.read_text())
-    rounds = 0
-    while True:
-        rounds += 1
-        next_layout = simulate_seating(current_layout)
-        if encode_layout(current_layout) == encode_layout(next_layout):
-            break
-        current_layout = next_layout
-    print(f'Converged after {rounds} rounds')
-    print('Problem 1: ', count_occupied_seats(current_layout))
+    for problem, count_method, max_tolerated_occupied_seats in (
+        (1, count_occupied_adjacent_seats, 4),
+        (2, count_occupied_visible_seats, 5),
+    ):
+        current_layout = parse_layout(DATASET.read_text())
+        rounds = 0
+        while True:
+            rounds += 1
+            next_layout = simulate_seating(current_layout, count_method, max_tolerated_occupied_seats)
+            if encode_layout(current_layout) == encode_layout(next_layout):
+                break
+            current_layout = next_layout
+        print(f'Converged after {rounds} rounds')
+        print(f'Problem {problem}: ', count_occupied_seats(current_layout))
 
 
 class State(Enum):
@@ -36,26 +40,31 @@ def encode_layout(layout: List[List[State]]) -> str:
     return '\n'.join(''.join(seat.value for seat in row) for row in layout)
 
 
-def simulate_seating(layout: List[List[State]]) -> List[List[State]]:
+def simulate_seating(layout: List[List[State]],
+                     count_method: Callable[[List[List[State]], int, int], int],
+                     max_tolerated_occupied_seats: int = 4) -> List[List[State]]:
     num_rows = len(layout)
     num_cols = len(layout[0])
     return [
         [
-            update_seat_state(layout, row_idx, col_idx)
+            update_seat_state(layout, row_idx, col_idx, count_method,
+                              max_tolerated_occupied_seats=max_tolerated_occupied_seats)
             for col_idx in range(num_cols)
         ] for row_idx in range(num_rows)
     ]
 
 
-def update_seat_state(layout: List[List[State]], row_idx: int, col_idx: int) -> State:
+def update_seat_state(layout: List[List[State]], row_idx: int, col_idx: int,
+                      count_method: Callable[[List[List[State]], int, int], int],
+                      max_tolerated_occupied_seats: int = 4) -> State:
     current_state = layout[row_idx][col_idx]
     if current_state == State.floor:
         return State.floor
 
-    num_occupied_adajcent = count_occupied_adjacent_seats(layout, row_idx, col_idx)
+    num_occupied_adajcent = count_method(layout, row_idx, col_idx)
     if current_state == State.empty and num_occupied_adajcent == 0:
         return State.occupied
-    elif current_state == State.occupied and num_occupied_adajcent >= 4:
+    elif current_state == State.occupied and num_occupied_adajcent >= max_tolerated_occupied_seats:
         return State.empty
     else:
         return current_state
@@ -79,6 +88,33 @@ def iter_adjacent_seats(layout: List[List[State]], row_idx: int, col_idx: int) -
                     not (row_delta == 0 and col_delta == 0)
             ):
                 yield layout[adjacent_row_idx][adjacent_col_idx]
+
+
+def count_occupied_visible_seats(layout: List[List[State]], row_idx: int, col_idx: int) -> int:
+    return sum(1 for adjacent_seat in iter_visible_seats(layout, row_idx, col_idx)
+               if adjacent_seat == State.occupied)
+
+
+def iter_visible_seats(layout: List[List[State]], row_idx: int, col_idx: int) -> Iterable[State]:
+    # the iter_adjacent_seats could be considered a special case of this one with max_steps = 1
+    num_rows = len(layout)
+    num_cols = len(layout[0])
+    for row_delta in (-1, 0, 1):
+        for col_delta in (-1, 0, 1):
+            if row_delta == 0 and col_delta == 0:
+                continue
+            steps = 1
+            while True:
+                visible_row_idx = row_idx + steps*row_delta
+                visible_col_idx = col_idx + steps*col_delta
+                if not (0 <= visible_row_idx < num_rows and 0 <= visible_col_idx < num_cols):
+                    break
+                state = layout[visible_row_idx][visible_col_idx]
+                if state != State.floor:
+                    yield state
+                    break
+                else:
+                    steps += 1
 
 
 def count_occupied_seats(layout: List[List[State]]) -> int:
--- day11/test_day11.py
+++ day11/test_day11.py
@@ -1,4 +1,6 @@
-from problem import count_occupied_adjacent_seats, count_occupied_seats, encode_layout, parse_layout, simulate_seating
+import pytest
+
+from problem import count_occupied_adjacent_seats, count_occupied_visible_seats, count_occupied_seats, encode_layout, parse_layout, simulate_seating
 
 LAYOUT_STEPS = [
 """\
@@ -75,22 +77,147 @@ L.#.L..#..
 """,
 ]
 
+LAYOUT_STEPS_PROBLEM_2 = [
+"""\
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL\
+""",
+"""\
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##\
+""",
+"""\
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#\
+""",
+"""\
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#\
+""",
+"""\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#\
+""",
+"""\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#\
+""",
+"""\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#\
+"""
+]
+
+EXAMPLE_VISIBLE_SEATS = [
+(4, 3, """\
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....\
+""", 8),
+(1, 1, """\
+.............
+.L.L.#.#.#.#.
+.............\
+""", 0),
+(3, 3, """\
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.\
+""", 0),
+]
+
 
 def test_day11_count_adjacent_occupied_seats():
     layout = parse_layout(LAYOUT_STEPS[1])
     assert count_occupied_adjacent_seats(layout, 0, 2) == 4
 
 
-def test_day11_simulate_seating():
-    prev_layout = parse_layout(LAYOUT_STEPS[0])
-    for round, expected_layout_text in enumerate(LAYOUT_STEPS[1:]):
+@pytest.mark.parametrize('row_idx,col_idx,layout_text,num_visible', EXAMPLE_VISIBLE_SEATS)
+def test_day11_count_occupied_visible_seats(row_idx, col_idx, layout_text, num_visible):
+    layout = parse_layout(layout_text)
+    assert count_occupied_visible_seats(layout, row_idx, col_idx) == num_visible
+
+
+@pytest.mark.parametrize('layout_steps,count_method,max_tolerated_occupied_seats', [
+    (LAYOUT_STEPS, count_occupied_adjacent_seats, 4),
+    (LAYOUT_STEPS_PROBLEM_2, count_occupied_visible_seats, 5),
+])
+def test_day11_simulate_seating(layout_steps, count_method, max_tolerated_occupied_seats):
+    prev_layout = parse_layout(layout_steps[0])
+    for round, expected_layout_text in enumerate(layout_steps[1:]):
         print(f'round {round + 1}...')
         expected_layout = parse_layout(expected_layout_text)
-        output = simulate_seating(prev_layout)
+        output = simulate_seating(prev_layout, count_method, max_tolerated_occupied_seats)
         print(encode_layout(output))
         assert output == expected_layout
         prev_layout = expected_layout
-    assert simulate_seating(prev_layout) == prev_layout
+    assert simulate_seating(prev_layout, count_method, max_tolerated_occupied_seats) == prev_layout
 
 
 def test_day11_count_occupied_seats():
