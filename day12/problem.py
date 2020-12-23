a4f30dc3bc75e582da3bf0df9fc78a33cfee2670
Stephen Helms <stephenhelms@gmail.com>
2020-12-23 20:02:29 +0100

    day 12 problem 2

--- day12/problem.py
+++ day12/problem.py
@@ -8,10 +8,10 @@ DATASET = Path(__file__).parent / 'dataset.txt'
 
 def main():
     commands = parse_commands(DATASET.read_text())
-    state = ShipState(0, 0, 0)
-    for command in commands:
-        state = state.execute_command(command)
-    print('Problem 1: ', state.manhattan_distance)
+    for problem_idx, state in enumerate((ShipState(0, 0, 0), ShipStateProblem2(0, 0, 10, 1))):
+        for command in commands:
+            state = state.execute_command(command)
+        print(f'Problem {problem_idx + 1}: ', state.manhattan_distance)
 
 
 class Command(NamedTuple):
@@ -19,11 +19,17 @@ class Command(NamedTuple):
     value: int
 
 
-@dataclass
 class ShipState:
-    x: int
-    y: int
-    orientation: int
+    def __init__(self, x: int, y: int, orientation: int):
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+
+    def __repr__(self):
+        return ','.join(map(str, (self.x, self.y, self.orientation)))
+
+    def __eq__(self, other):
+        return (self.x, self.y, self.orientation) == (other.x, other.y, other.orientation)
 
     @property
     def manhattan_distance(self) -> int:
@@ -39,14 +45,81 @@ class ShipState:
         elif command.action == 'W':
             return ShipState(self.x - command.value, self.y, self.orientation)
         elif command.action == 'L':
-            return ShipState(self.x, self.y, wrap_angle(self.orientation + command.value))
+            return ShipState(self.x, self.y, self._wrap_angle(self.orientation + command.value))
         elif command.action == 'R':
-            return ShipState(self.x, self.y, wrap_angle(self.orientation - command.value))
+            return ShipState(self.x, self.y, self._wrap_angle(self.orientation - command.value))
         elif command.action == 'F':
             return ShipState(
                 round(self.x + math.cos(self.orientation * 2 * math.pi / 360) * command.value),
                 round(self.y + math.sin(self.orientation * 2 * math.pi / 360) * command.value),
                 self.orientation
+            )
+        else:
+            raise ValueError(f'Unknown action: {command.action}')
+
+    @staticmethod
+    def _wrap_angle(angle: int) -> int:
+        if angle < 0:
+            return 360 + angle
+        elif angle > 360:
+            return angle - 360
+        else:
+            return angle
+
+
+class ShipStateProblem2(ShipState):
+    def __init__(self, x: int, y: int, waypoint_x: int, waypoint_y: int):
+        self.x = x
+        self.y = y
+        self.waypoint_x = waypoint_x
+        self.waypoint_y = waypoint_y
+
+    def __repr__(self):
+        return ','.join(map(str, (self.x, self.y, self.waypoint_x, self.waypoint_y)))
+
+    def __eq__(self, other):
+        return (
+                (self.x, self.y, self.waypoint_x, self.waypoint_y) ==
+                (other.x, other.y, other.waypoint_x, other.waypoint_y)
+        )
+
+    @property
+    def orientation(self) -> float:
+        return math.atan2(self.waypoint_y, self.waypoint_x) / (2 * math.pi) * 360
+
+    @property
+    def waypoint_distance(self) -> float:
+        return math.sqrt(self.waypoint_x ** 2 + self.waypoint_y ** 2)
+
+    def execute_command(self, command: Command) -> 'ShipStateProblem2':
+        if command.action == 'N':
+            return ShipStateProblem2(self.x, self.y, self.waypoint_x, self.waypoint_y + command.value)
+        elif command.action == 'S':
+            return ShipStateProblem2(self.x, self.y, self.waypoint_x, self.waypoint_y - command.value)
+        elif command.action == 'E':
+            return ShipStateProblem2(self.x, self.y, self.waypoint_x + command.value, self.waypoint_y)
+        elif command.action == 'W':
+            return ShipStateProblem2(self.x, self.y, self.waypoint_x - command.value, self.waypoint_y)
+        elif command.action == 'L':
+            new_orientation_rads = (self.orientation + command.value) * 2 * math.pi / 360
+            return ShipStateProblem2(
+                self.x, self.y,
+                round(math.cos(new_orientation_rads) * self.waypoint_distance),
+                round(math.sin(new_orientation_rads) * self.waypoint_distance)
+            )
+        elif command.action == 'R':
+            new_orientation_rads = (self.orientation - command.value) * 2 * math.pi / 360
+            return ShipStateProblem2(
+                self.x, self.y,
+                round(math.cos(new_orientation_rads) * self.waypoint_distance),
+                round(math.sin(new_orientation_rads) * self.waypoint_distance)
+            )
+        elif command.action == 'F':
+            return ShipStateProblem2(
+                self.x + self.waypoint_x * command.value,
+                self.y + self.waypoint_y * command.value,
+                self.waypoint_x,
+                self.waypoint_y
             )
         else:
             raise ValueError(f'Unknown action: {command.action}')
@@ -57,15 +130,6 @@ def parse_commands(text: str) -> List[Command]:
         Command(line[0], int(line[1:]))
         for line in text.splitlines(False)
     ]
-
-
-def wrap_angle(angle: int) -> int:
-    if angle < 0:
-        return 360 + angle
-    elif angle > 360:
-        return angle - 360
-    else:
-        return angle
 
 
 if __name__ == '__main__':
--- day12/test_day12.py
+++ day12/test_day12.py
@@ -1,4 +1,6 @@
-from problem import parse_commands, ShipState
+import pytest
+
+from problem import parse_commands, ShipState, ShipStateProblem2
 
 COMMANDS = """\
 F10
@@ -15,11 +17,21 @@ EXPECTED_STATES = [
     ShipState(17, 3, 270),
     ShipState(17, -8, 270),
 ]
+EXPECTED_STATES_2 = [
+    ShipStateProblem2(100, 10, 10, 1),
+    ShipStateProblem2(100, 10, 10, 4),
+    ShipStateProblem2(170, 38, 10, 4),
+    ShipStateProblem2(170, 38, 4, -10),
+    ShipStateProblem2(214, -72, 4, -10)
+]
 
 
-def test_day11_commands_state():
-    state = ShipState(0, 0, 0)
-    for idx, (command, expected_state) in enumerate(zip(parse_commands(COMMANDS), EXPECTED_STATES)):
+@pytest.mark.parametrize('state,expected_states', [
+    (ShipState(0, 0, 0), EXPECTED_STATES),
+    (ShipStateProblem2(0, 0, 10, 1), EXPECTED_STATES_2),
+])
+def test_day11_commands_state(state, expected_states):
+    for idx, (command, expected_state) in enumerate(zip(parse_commands(COMMANDS), expected_states)):
         print(idx)
         state = state.execute_command(command)
         assert state == expected_state
