761918854761e808b42153101637d6e0a3336554
Stephen Helms <stephenhelms@gmail.com>
2020-12-27 20:38:12 +0100

    day 14 problem 2

--- day14/problem.py
+++ day14/problem.py
@@ -1,13 +1,16 @@
+from functools import reduce
+import itertools as it
 from pathlib import Path
 import re
-from typing import List, Tuple
+from typing import Iterable, List, Tuple
 
 DATASET = Path(__file__).parent / 'dataset.txt'
 
 
 def main():
     cmds = parse_program(DATASET.read_text())
-    print('Problem 1: ', run_program(cmds))
+    print('Problem 1: ', run_program_v1(cmds))
+    print('Problem 2: ', run_program_v2(cmds))
 
 
 def parse_program(text: str) -> List[Tuple[str, int, int]]:
@@ -18,28 +21,59 @@ def parse_program(text: str) -> List[Tuple[str, int, int]]:
     for line in text.splitlines(False):
         mask_match = mask_pattern.match(line)
         if mask_match:
-            mask = mask_match.group(0)
+            mask = mask_match.group(1)
         else:
             mem_pos, value = cmd_pattern.match(line).groups()
             cmds.append((mask, int(mem_pos), int(value)))
     return cmds
 
 
-def run_program(commands: List[Tuple[str, int, int]]):
+def run_program_v1(commands: List[Tuple[str, int, int]]):
     memory = {}
     for mask, pos, value in commands:
-        memory[pos] = apply_bitmask(mask, value)
+        memory[pos] = apply_bitmask_v1(mask, value)
     return sum(memory.values())
 
 
-def apply_bitmask(mask: str, value: int) -> int:
+def apply_bitmask_v1(mask: str, value: int) -> int:
     for idx, mask_value in enumerate(reversed(mask)):
         if mask_value == 'X':
             continue
-        bitmask = 1 << idx
-        value &= ~bitmask
-        if mask_value == '1':
-            value |= bitmask
+        value = set_bit(value, idx, mask_value == '1')
+    return value
+
+
+def run_program_v2(commands: List[Tuple[str, int, int]]):
+    memory = {}
+    for mask, pos, value in commands:
+        for mem_pos in apply_bitmask_v2(mask, pos):
+            memory[mem_pos] = value
+    return sum(memory.values())
+
+
+def apply_bitmask_v2(mask: str, value: int) -> Iterable[int]:
+    floating_bits = []
+    for idx, mask_value in enumerate(reversed(mask)):
+        if mask_value == '0':
+            continue
+        elif mask_value == '1':
+            value = set_bit(value, idx, True)
+        else:
+            floating_bits.append(idx)
+    yield reduce(lambda value, bit: set_bit(value, bit, False), floating_bits, value)
+    for num_true_bits in range(1, len(floating_bits) + 1):
+        for true_bits in it.combinations(floating_bits, num_true_bits):
+            floating_value = value
+            for bit in floating_bits:
+                floating_value = set_bit(floating_value, bit, bit in true_bits)
+            yield floating_value
+
+
+def set_bit(value: int, bit: int, enabled: bool) -> int:
+    bitmask = 1 << bit
+    value &= ~bitmask
+    if enabled:
+        value |= bitmask
     return value
 
 
--- day14/test_day14.py
+++ day14/test_day14.py
@@ -1,12 +1,18 @@
 import pytest
 
-from problem import apply_bitmask, parse_program, run_program
+from problem import apply_bitmask_v1, apply_bitmask_v2, parse_program, run_program_v1, run_program_v2
 
-PROGRAM = """\
+PROGRAM_V1 = """\
 mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0\
+"""
+PROGRAM_V2 = """\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1\
 """
 
 
@@ -16,10 +22,23 @@ mem[8] = 0\
     (101, 101),
     (0, 64),
 ])
-def test_apply_bitmask(mask, value, result):
-    assert apply_bitmask(mask, value) == result
+def test_apply_bitmask_v1(mask, value, result):
+    assert apply_bitmask_v1(mask, value) == result
 
 
-def test_day14_run_program():
-    cmds = parse_program(PROGRAM)
-    assert run_program(cmds) == 165
+def test_day14_run_program_v1():
+    cmds = parse_program(PROGRAM_V1)
+    assert run_program_v1(cmds) == 165
+
+
+@pytest.mark.parametrize('mask,value,result', [
+    ('000000000000000000000000000000X1001X', 42, {26, 27, 58, 59}),
+    ('00000000000000000000000000000000X0XX', 26, {16, 17, 18, 19, 24, 25, 26, 27})
+])
+def test_apply_bitmask_v2(mask, value, result):
+    assert set(apply_bitmask_v2(mask, value)) == result
+
+
+def test_day14_run_program_v2():
+    cmds = parse_program(PROGRAM_V2)
+    assert run_program_v2(cmds) == 208
