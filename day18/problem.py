46fb506bec73d6e2106225004f3367cacec87918
Stephen Helms <stephenhelms@gmail.com>
2021-01-17 11:57:08 +0100

    day 18 problem 2

--- day18/problem.py
+++ day18/problem.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import re
-from typing import Iterable
+from typing import Iterable, Callable
 
 DATASET = Path(__file__).parent / 'dataset.txt'
 
@@ -8,21 +8,26 @@ DATASET = Path(__file__).parent / 'dataset.txt'
 def main():
     expressions = DATASET.read_text().splitlines(False)
     print('Problem 1: ', sum(eval_expression(expression) for expression in expressions))
+    print('Problem 2: ', sum(eval_expression_problem2(expression) for expression in expressions))
 
 
 def eval_expression(expression: str) -> int:
+    expression = _eval_subexpressions(expression, eval_expression)
+    return _eval_left_to_right(expression)
+
+
+def eval_expression_problem2(expression: str) -> int:
+    expression = _eval_subexpressions(expression, eval_expression_problem2)
+    expression = _eval_addition(expression)
+    return _eval_left_to_right(expression)
+
+
+def _eval_subexpressions(expression, eval_func: Callable[[str], int]) -> str:
     while '(' in expression:
         for subexpression in find_subexpressions(expression):
-            expression = expression.replace(subexpression, str(eval_expression(subexpression[1:-1])))
-    value = 0
-    operation = '+'
-    for token in expression.split(' '):
-        if token in ('+', '-', '*'):
-            operation = token
-        else:
-            next_value = int(token)
-            value = eval(f'{value} {operation} {next_value}')
-    return value
+            expression = expression.replace(subexpression,
+                                            str(eval_func(subexpression[1:-1])))
+    return expression
 
 
 def find_subexpressions(expression: str) -> Iterable[str]:
@@ -30,6 +35,25 @@ def find_subexpressions(expression: str) -> Iterable[str]:
     for match in pattern.finditer(expression):
         yield match.group(0)
 
+
+def _eval_addition(expression: str) -> str:
+    while '+' in expression:
+        for match in re.finditer(r'\d+ \+ \d+', expression):
+            subexpression = match.group(0)
+            expression = expression.replace(subexpression, str(eval(subexpression)), 1)
+    return expression
+
+
+def _eval_left_to_right(expression: str) -> int:
+    value = 0
+    operation = '+'
+    for token in expression.split(' '):
+        if token in ('+', '*'):
+            operation = token
+        else:
+            next_value = int(token)
+            value = eval(f'{value} {operation} {next_value}')
+    return value
 
 
 if __name__ == '__main__':
--- day18/test_day18.py
+++ day18/test_day18.py
@@ -1,6 +1,6 @@
 import pytest
 
-from problem import eval_expression, find_subexpressions
+from problem import eval_expression, eval_expression_problem2, find_subexpressions
 
 
 @pytest.mark.parametrize('expression,result', [
@@ -9,6 +9,19 @@ from problem import eval_expression, find_subexpressions
 ])
 def test_day18_eval_expression(expression, result):
     assert eval_expression(expression) == result
+
+
+@pytest.mark.parametrize('expression,result', [
+    ('1 + 2 * 3 + 4 * 5 + 6', 231),
+    ('1 + (2 * 3) + (4 * (5 + 6))', 51),
+    ('2 * 3 + (4 * 5)', 46),
+    ('5 + (8 * 3 + 9 + 3 * 4 * 3)', 1445),
+    ('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 669060),
+    ('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 23340),
+    ('8 + 3 * ((4 * 8 * 5 * 5) + (8 * 9 * 8 + 7 * 9)) + (4 * 7) + 3 * 4', 464244)
+])
+def test_day18_eval_expression_problem2(expression, result):
+    assert eval_expression_problem2(expression) == result
 
 
 @pytest.mark.parametrize('expression,results', [
